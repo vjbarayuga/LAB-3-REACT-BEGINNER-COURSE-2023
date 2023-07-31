d770945a481980a7d0eb2a2120de678e392c3e57
vjdbarayuga <vjdbarayuga@gmail.com>
2023-07-31 18:13:59 +0800

    episode4 with reactjs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  // This only applies to vanilla javascript
+  // let age = 0;
+  
+  // const increaseAge = () => {
+  //   age = age + 1;
+  //   console.log(age);
+  // };
+
   const [count, setCount] = useState(0);
 
   const increase = () => {
@@ -14,6 +22,18 @@ function App() {
     setCount(0);
   };
 
+  //state for input value
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (event) => {
+    //console.log(event.target.value);
+    setInputValue(event.target.value);
+
+  };
+
+  //state for show or hide text
+  const [showText, setShowText] = useState(true);
+
+
   return (
     <div className="App">
       <button onClick={increase}>Increase</button>
@@ -21,6 +41,16 @@ function App() {
       <button onClick={setToZero}>Set to Zero</button>
 
       {count}
+
+      {/* This only applies to vanilla javascript */}
+      {/* {age}
+      <button onClick={increaseAge}> Increase Age </button> */}
+
+      <input type="text" onChange={handleInputChange}/>
+      {inputValue}
+
+      <button onClick={() => {setShowText(!showText)}}> Show/Hide Text </button>
+      {showText === true && <h1> HI MY NAME IS VJ </h1>}
     </div>
   );
 }
